4660238fd810289c732098f5e2af1d9671f2b197
Kaue Pereira <kauefontes@outlook.com>
2024-06-29 23:31:06 -0400

    fix: fix ownership issue

--- src/configuration.rs
+++ src/configuration.rs
@@ -84,8 +84,8 @@ impl TryFrom<String> for Environment {
 
 impl DatabaseSettings {
     pub fn with_db(&self) -> PgConnectOptions {
-        let mut options = self.without_db().database(&self.database_name);
-        options.log_statements(tracing_log::log::LevelFilter::Trace);
+        let options = self.without_db().database(&self.database_name);
+        options.clone().log_statements(tracing_log::log::LevelFilter::Trace);
         options
     }
 
